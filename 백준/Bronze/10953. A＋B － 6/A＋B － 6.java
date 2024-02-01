4b1ece8db8d2040d155f85c021cee924d9dd2f2f
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-02-02 02:03:06 +0900

    [Bronze III] Title: A+B - 6, Time: 216 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    	StringTokenizer st;
+    	String str;
+    	
+    	int t = sc.nextInt();
+    	
+    	for(int i=0; i<t; i++) {
+    		str=sc.next();
+    		st = new StringTokenizer(str,",");
+    		
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		System.out.println(a+b);
+    		
+    	}
+    	sc.close();
+    	
+    }
+}
