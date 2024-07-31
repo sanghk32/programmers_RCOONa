9aba04503a5d47dbd9b282b7eaa21a60293e0dec
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-07-31 21:21:28 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        Map<String, String> map = new HashMap<>();
+        map.put("NLCS", "North London Collegiate School");
+        map.put("BHA", "Branksome Hall Asia");
+        map.put("KIS", "Korea International School");
+        map.put("SJA", "St. Johnsbury Academy");
+        System.out.println(map.get(n));
+    }
+}
