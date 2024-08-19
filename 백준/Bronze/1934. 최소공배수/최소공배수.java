9ce640a19bb79bf56fd04b57337b3066da42da06
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-08-20 00:02:57 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 15028 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            //최대공약수 구함.
+            System.out.println(((a * b) / gcd(a, b)));
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+}
+
