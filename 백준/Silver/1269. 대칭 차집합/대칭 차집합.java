2c76f59f0a5cec621c92851683b0d7b48bbfca84
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-08-23 00:43:01 +0900

    [Silver IV] Title: 대칭 차집합, Time: 696 ms, Memory: 95132 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -7,46 +6,46 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Map<Integer,Integer> map;
-        Map<Integer,Integer> map2;
+        Map<Integer, Integer> map;
+        Map<Integer, Integer> map2;
 
 
         int count = 0;
 
-        st= new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        int n= Integer.parseInt(st.nextToken());
-        int m= Integer.parseInt(st.nextToken());
-        int[]arr = new int[n];
-        int[]arr2 = new int[m];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] arr2 = new int[m];
 
         map = new HashMap<>();
-        st= new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            arr[i]=num;
-            map.put(num,i);
+            arr[i] = num;
+            map.put(num, i);
         }
 
         map2 = new HashMap<>();
-        st= new StringTokenizer(br.readLine());
-        for(int j=0; j<m; j++){
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < m; j++) {
             int num = Integer.parseInt(st.nextToken());
-            arr2[j]=num;
-            map2.put(num,num);
+            arr2[j] = num;
+            map2.put(num, num);
         }
 
-        for(int k=0; k<map.size(); k++){ //3
-            if(!map2.containsKey(arr[k])){
+        for (int k = 0; k < map.size(); k++) { //3
+            if (!map2.containsKey(arr[k])) {
                 count++;
             }
         }
 
-        for(int z=0; z<map2.size(); z++){
-            if(!map.containsKey(arr2[z])){
+        for (int z = 0; z < map2.size(); z++) {
+            if (!map.containsKey(arr2[z])) {
                 count++;
             }
         }
