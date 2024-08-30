795409654d7691681f1dd268711eda639b7bb5bb
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-08-31 02:23:25 +0900

    [Bronze V] Title: 크냐?, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -1,26 +1,21 @@
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         while (true) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            if (a == 0 && b == 0) {
-                break;
-            } else if (a > b) {
-                System.out.println("Yes");
-            } else if (a < b) {
-                System.out.println("No");
-            } else if (a == b) {
-                System.out.println("No");
+            if (a == 0 && b == 0) break;  //a, b 둘다 0이면 loop 종료
+            if (a > b) {
+                sb.append("Yes\n");
+            } else {
+                sb.append("No\n");
             }
         }
+        System.out.print(sb);
     }
 }
