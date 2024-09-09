96e4b6bfb1bf363936e1537d4140f6ef1888cb95
poppyee <129316014+sanghk32@users.noreply.github.com>
2024-09-09 23:07:28 +0900

    [Bronze V] Title: 검증수, Time: 96 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,23 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            int n = Integer.parseInt(st.nextToken());
+            arr[i] = n * n;
+        }
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+        }
+        System.out.println(sum % 10);
+    }
+}
