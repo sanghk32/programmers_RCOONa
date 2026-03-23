d46023d32d7cf53ccd95180a290eb1bd26f0148a
poppyee <129316014+sanghk32@users.noreply.github.com>
2026-03-23 11:47:09 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 120 ms, Memory: 20608 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
+
+        Deque<Integer> q = new LinkedList<>();
+        boolean[] visit = new boolean[100001];
+        int dist[] = new int[100001];
+
+        
+        q.add(N);
+        visit[N] = true; 
+        
+        while(!q.isEmpty()){
+    int x = q.poll();
+
+    if(x==M){
+        System.out.println(dist[x]);
+        return; 
+    }
+
+    int[] next = {x-1 ,x+1, x*2};
+
+    for(int i=0; i<3; i++){
+        int nx = next[i];
+
+
+        if(nx>=0 && nx<=100000 && !visit[nx]){
+
+            if(nx == x*2){
+                dist[nx] = dist[x];
+                q.addFirst(nx);
+                visit[nx] = true;
+            }else{
+                dist[nx] = dist[x] + 1;
+                q.add(nx);
+                visit[nx] = true;
+
+            }
+        }
+    }
+        }
+    }
+}
