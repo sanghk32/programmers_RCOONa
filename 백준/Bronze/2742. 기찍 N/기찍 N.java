012bc232335ea5baf4d03f5a01058242c3e9075a
poppyee <129316014+sanghk32@users.noreply.github.com>
2026-03-25 23:42:51 +0900

    [Bronze V] Title: 기찍 N, Time: 1080 ms, Memory: 30504 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=n; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
