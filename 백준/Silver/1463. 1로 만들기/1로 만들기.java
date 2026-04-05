e906ee5d49bedc0a115ea993b3b94da587b45e6a
poppyee <129316014+sanghk32@users.noreply.github.com>
2026-04-05 21:28:36 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15492 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[x+1];
+        for(int i=2; i<=x; i++){ 
+            dp[i] = dp[i-1] + 1;
+
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+        }
+        System.out.print(dp[x]);
+    }
+}
